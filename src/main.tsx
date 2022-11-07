519a6821bb1976a5c37a407b57226219bd3de3fe
BobbyKJH <rlatnguscjsw@gmail.com>
2022-11-07 16:35:23 +0900

    Feat: React-query connection
    1) 내용
    
    -  react-query 연결
    -  ReactQueryDevtools 연결
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import { RecoilRoot } from "recoil";
 
 import { GlobalStyle } from "./style/common/Global.styled";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RecoilRoot>
-    <GlobalStyle />
-    <App />
-  </RecoilRoot>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </RecoilRoot>
+  </QueryClientProvider>
 );
