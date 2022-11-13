c543088a9c6ae80bee95287142a246e27b3aa445
BobbyKJH <rlatnguscjsw@gmail.com>
2022-11-13 16:41:08 +0900

    Style: SideBarContent 스타일
    1) 내용
    
    -  SideBarContent 스타일 생성 및 적용
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/style/common/SideBarContent.styled.ts
+++ src/style/common/SideBarContent.styled.ts
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
-import { ListItemButton, Toolbar } from "@mui/material";
+import { Button, ListItemButton, ListItemText, Toolbar } from "@mui/material";
 
-export const ListLink = styled(NavLink)`
+export const SideLink = styled(NavLink)`
   background-color: #fff;
   color: #009223;
   align-content: center;
@@ -11,16 +11,48 @@ export const ListLink = styled(NavLink)`
   }
 `;
 
-export const ListButton = styled(ListItemButton)`
+export const SideButton = styled(ListItemButton)`
   display: flex;
   align-items: center;
 
-  padding: 20px 10px;
+  padding: 15px 10px;
   &:hover {
     background-color: #ffce32;
   }
 `;
 
+export const SideMenu = styled.button`
+  background-color: #fff;
+  color: #009223;
+  align-content: center;
+  margin: 0;
+  padding: 0;
+  border: none;
+  .path {
+    background-color: #ffce32;
+  }
+`;
+
+export const MenuKindButton = styled(Button)`
+  width: 100%;
+  text-align: left;
+  padding: 12px 10px;
+  padding-left: 48px;
+  color: #808080;
+  span {
+    font-weight: 900;
+    font-family: "Gothic A1", sans-serif;
+    font-size: 0.8rem;
+  }
+`;
+
 export const NickToolbar = styled(Toolbar)`
   justify-content: center;
+`;
+
+export const SideText = styled(ListItemText)`
+  span {
+    font-family: "Gothic A1", sans-serif;
+    font-weight: 900;
+  }
 `;
