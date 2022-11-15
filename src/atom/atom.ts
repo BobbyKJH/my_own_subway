1bbc04ae68a7119156f30ed8c74528cf77c38c67
BobbyKJH <rlatnguscjsw@gmail.com>
2022-11-16 00:10:36 +0900

    Feat: atom 생성
    1) 내용
    
    -  menuList sessionStorage를 이용하여 메뉴 불러오는 기능 저장
    -  nick을 localStorage에 저장 atom 생성
    -  atomFamily을 이용하여 나만의 레시피 만드는 시스템 생성
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/atom/atom.ts
+++ src/atom/atom.ts
@@ -1,4 +1,28 @@
-import { atom } from "recoil";
+import { atom, atomFamily, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+interface IMake {
+  img: string | undefined;
+  name: string | undefined;
+  eng_name: string | undefined;
+  calorie: number | undefined;
+}
+
+const { persistAtom: menuStorage } = recoilPersist({
+  key: "menu/List",
+  storage: sessionStorage,
+});
+
+const { persistAtom: nickStorage } = recoilPersist({
+  key: "logIn/NickName",
+  storage: localStorage,
+});
+
+export const nickSetting = atom({
+  key: "setting/Nick",
+  default: "",
+  effects: [nickStorage],
+});
 
 export const sideBarOpen = atom({
   key: "mobile",
@@ -6,6 +30,41 @@ export const sideBarOpen = atom({
 });
 
 export const menuUrl = atom({
-  key: "menuUrl",
+  key: "menu/Url",
   default: "sandwich",
+  effects: [menuStorage],
+});
+
+export const makeSequence = atom({
+  key: "make/Sequence",
+  default: "1",
+});
+
+export const recipeFamily = atomFamily({
+  key: "make/Recipe",
+  default: (menu): IMake => {
+    return {
+      img: "",
+      name: "",
+      eng_name: "",
+      calorie: 0,
+    };
+  },
+});
+
+export const recipeCalorie = selector({
+  key: "recipe/calorie",
+  get: ({ get }) => {
+    const sandwich = get(recipeFamily("sandwich")).calorie;
+    const bread = get(recipeFamily("bread")).calorie;
+    const cheese = get(recipeFamily("cheese")).calorie;
+    const sauce = get(recipeFamily("sauce")).calorie;
+    return (
+      sandwich !== undefined &&
+      bread !== undefined &&
+      cheese !== undefined &&
+      sauce !== undefined &&
+      sandwich + bread + cheese + sauce
+    );
+  },
 });
