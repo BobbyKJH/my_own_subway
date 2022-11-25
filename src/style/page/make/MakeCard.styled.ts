268768e387bbd6fb210bd17015b99931b4710542
BobbyKJH <rlatnguscjsw@gmail.com>
2022-11-25 18:58:45 +0900

    Style: MakeCard Style
    1) 내용
    
    -  선택된 메뉴 className Style 추가
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/style/page/make/MakeCard.styled.ts
+++ src/style/page/make/MakeCard.styled.ts
@@ -9,32 +9,40 @@ export const MakeCardContainer = styled(Card)`
   cursor: pointer;
   display: inline-block;
   max-width: 400px;
-  border: 2px solid #808080;
-  border-radius: 20px;
+  .select {
+    border: 5px solid #009223;
+  }
+  .card {
+    border: 5px solid #ffce32;
+  }
 `;
 
 export const MakeCardImage = styled.img`
   width: 100%;
+  background-color: #fff;
 `;
 
 export const MakeCardContent = styled.div`
-  padding: 0;
-  padding-top: 0;
+  padding: 5px 0;
 `;
 
 export const MakeCardName = styled(Typography)<IMakeCard>`
   font-family: "Gothic A1", sans-serif;
+  text-align: center;
   color: #009223;
   font-size: 1rem;
+  margin: 0;
   @media (max-width: 600px) {
     font-size: 0.7em;
   }
 `;
 
-export const MakeCardEng = styled(Typography)<IMakeCard>`
-  font-family: "Gothic A1", sans-serif;
+export const MakeCardEng = styled(MakeCardName)<IMakeCard>`
   color: #ffce32;
-  font-size: 0.5em;
+  font-size: 0.7em;
+  @media (max-width: 600px) {
+    font-size: 0.5em;
+  }
 `;
 
 export const MakeCardCalorie = styled(Typography)<IMakeCard>`
