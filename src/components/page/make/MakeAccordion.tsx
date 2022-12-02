a5ffdfd2571c2d792ffe4eb3be6485ddbe317c2a
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-02 19:33:56 +0900

    Refactor: MakeAccordion 변경
    1) 내용
    
    -  선택창 스타일 및 로직 변경
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/components/page/make/MakeAccordion.tsx
+++ src/components/page/make/MakeAccordion.tsx
@@ -1,120 +1,21 @@
-import { SyntheticEvent } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
-import { useRecoilState } from "recoil";
-import { recipeOpen } from "../../../atom/atom";
+const steps = ["샌드위치", "빵", "치즈", "소스", "결과"];
 
-import MakeList from "./MakeList";
-import MakeResult from "./MakeReult";
-
-import {
-  MakeAccordionBox,
-  MakeAccordionContainer,
-  MakeAccordionDetails,
-  MakeAccordionSummary,
-} from "../../../style/page/make/MakeAccordion.styled";
-
-const MakeAccordion = () => {
-  const [open, setOpen] = useRecoilState(recipeOpen);
-
-  const handleChange =
-    (panel: string) => (e: SyntheticEvent, newExpanded: boolean) => {
-      setOpen(newExpanded ? panel : false);
-    };
-
+export default function MakeAccordion() {
   return (
-    <MakeAccordionContainer>
-      <MakeAccordionBox
-        disableGutters
-        elevation={0}
-        square
-        expanded={open === "sandwich"}
-        onChange={handleChange("sandwich")}
-      >
-        <MakeAccordionSummary
-          className={open === "sandwich" ? "select-menu" : "select-completion"}
-        >
-          <p>1. 샌드위치</p>
-        </MakeAccordionSummary>
-
-        <MakeAccordionDetails>
-          <MakeList make="sandwich" next="bread" />
-        </MakeAccordionDetails>
-      </MakeAccordionBox>
-
-      <MakeAccordionBox
-        disableGutters
-        elevation={0}
-        square
-        expanded={open === "bread"}
-        onChange={handleChange("bread")}
-      >
-        <MakeAccordionSummary
-          className={open === "bread" ? "select-menu" : "select-completion"}
-        >
-          <p>2. 빵</p>
-        </MakeAccordionSummary>
-
-        <MakeAccordionDetails>
-          <MakeList make="bread" next="cheese" />
-        </MakeAccordionDetails>
-      </MakeAccordionBox>
-
-      <MakeAccordionBox
-        disableGutters
-        elevation={0}
-        square
-        expanded={open === "cheese"}
-        onChange={handleChange("cheese")}
-      >
-        <MakeAccordionSummary
-          className={open === "cheese" ? "select-menu" : "select-completion"}
-        >
-          <p>3. 치즈</p>
-        </MakeAccordionSummary>
-
-        <MakeAccordionDetails>
-          <MakeList make="cheese" next="sauce" />
-        </MakeAccordionDetails>
-      </MakeAccordionBox>
-
-      <MakeAccordionBox
-        disableGutters
-        elevation={0}
-        square
-        expanded={open === "sauce"}
-        onChange={handleChange("sauce")}
-      >
-        <MakeAccordionSummary
-          className={open === "sauce" ? "select-menu" : "select-completion"}
-        >
-          <p>4. 소스</p>
-        </MakeAccordionSummary>
-
-        <MakeAccordionDetails>
-          <MakeList make="sauce" next="result" />
-        </MakeAccordionDetails>
-      </MakeAccordionBox>
-
-      <MakeAccordionBox
-        disableGutters
-        elevation={0}
-        square
-        expanded={open === "result"}
-        onChange={handleChange("result")}
-        TransitionProps={{ unmountOnExit: true }}
-      >
-        <MakeAccordionSummary
-          className={open === "result" ? "select-menu" : "select-completion"}
-        >
-          <p>5. 결과</p>
-        </MakeAccordionSummary>
-
-        <MakeAccordionDetails>
-          <MakeResult />
-        </MakeAccordionDetails>
-      </MakeAccordionBox>
-    </MakeAccordionContainer>
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={4} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
   );
-};
-
-export default MakeAccordion;
+}
