d0b369a90ebd12970b5f6d5f47b66b64da634110
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-03 18:08:47 +0900

    Feat: recipeOpen atom
    1) 내용
    
    -  MakeAccordion recipeOpen atom 적용
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/components/page/make/MakeAccordion.tsx
+++ src/components/page/make/MakeAccordion.tsx
@@ -1,4 +1,11 @@
-import * as React from "react";
+import { useEffect } from "react";
+
+import { useRecoilValue, useResetRecoilState } from "recoil";
+import { recipeOpen } from "../../../atom/atom";
+
+import MakeList from "./MakeList";
+import MakeSauce from "./MakeSauce";
+
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
@@ -6,16 +13,31 @@ import StepLabel from "@mui/material/StepLabel";
 
 const steps = ["샌드위치", "빵", "치즈", "소스", "결과"];
 
-export default function MakeAccordion() {
+const MakeAccordion = () => {
+  const select = useRecoilValue(recipeOpen);
+  const reset = useResetRecoilState(recipeOpen);
+
+  useEffect(() => {
+    reset();
+  }, []);
+
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={4} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
+    <>
+      <Box sx={{ width: "100%" }}>
+        <Stepper activeStep={select} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+      {select === 0 && <MakeList select="sandwich" />}
+      {select === 1 && <MakeList select="bread" />}
+      {select === 2 && <MakeList select="cheese" />}
+      {select === 3 && <MakeSauce />}
+    </>
   );
-}
+};
+
+export default MakeAccordion;
