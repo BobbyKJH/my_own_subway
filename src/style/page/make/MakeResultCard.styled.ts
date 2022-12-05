bc564cfea75666679f6c763b4ad440bd51f6e75a
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-05 18:53:17 +0900

    Style: MakeResultCard Style 생성
    1) 내용
    
    -  이미지, 이름, 영문명 Style 생성
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

---
+++ src/style/page/make/MakeResultCard.styled.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const MakeResultCardImg = styled.img`
+  display: block;
+  width: 50%;
+  margin: 0 auto;
+`;
+
+export const MakeResultCardName = styled.p`
+  text-align: center;
+  color: #009223;
+  font-size: 0.9rem;
+  line-height: 25px;
+`;
+
+export const MakeResultCardEng = styled(MakeResultCardName)`
+  color: #ffce32;
+  font-size: 0.7rem;
+`;
