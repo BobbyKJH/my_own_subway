bacfe9d1cc4d1681ee226afa4f3a79910e820aa2
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-07 00:01:45 +0900

    Feat: MakeAccordion select 수에 따른 렌더링 컴포넌트 변경
    1) 내용
    
    -  Accordion 변경으로 인하여 다시 생성
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/components/page/make/MakeAccordion.tsx
+++ src/components/page/make/MakeAccordion.tsx
@@ -1,20 +1,22 @@
 import { useEffect } from "react";
 
-import { useRecoilValue, useResetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { recipeOpen } from "../../../atom/atom";
 
 import MakeList from "./MakeList";
-import MakeSauce from "./MakeSauce";
+import MakeSauceList from "./MakeSauceList";
+import MakeResult from "./MakeReult";
 
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import { MakeAccordionContainer } from "../../../style/page/make/MakeAccordion.styled";
 
 const steps = ["샌드위치", "빵", "치즈", "소스", "결과"];
 
 const MakeAccordion = () => {
-  const select = useRecoilValue(recipeOpen);
+  const [select, setSelect] = useRecoilState(recipeOpen);
   const reset = useResetRecoilState(recipeOpen);
 
   useEffect(() => {
@@ -23,19 +25,21 @@ const MakeAccordion = () => {
 
   return (
     <>
-      <Box sx={{ width: "100%" }}>
+      <MakeAccordionContainer>
         <Stepper activeStep={select} alternativeLabel>
           {steps.map((label) => (
             <Step key={label}>
-              <StepLabel>{label}</StepLabel>
+              <StepLabel sx={{ fontSize: "10rem" }}>{label}</StepLabel>
             </Step>
           ))}
         </Stepper>
-      </Box>
+      </MakeAccordionContainer>
+      <button onClick={() => setSelect((prev) => prev + 1)}>1111</button>
       {select === 0 && <MakeList select="sandwich" />}
       {select === 1 && <MakeList select="bread" />}
       {select === 2 && <MakeList select="cheese" />}
-      {select === 3 && <MakeSauce />}
+      {select === 3 && <MakeSauceList />}
+      {select === 4 && <MakeResult />}
     </>
   );
 };
