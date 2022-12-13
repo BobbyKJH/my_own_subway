b6c4789bb7489e6b215f56c1a52f92415749ccb1
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-14 00:13:53 +0900

    Style: MakeButton Style
    1) 내용
    
    -  MakeButton Style bgc && color 변경
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

---
+++ src/style/page/make/makeButton/MakeButton.styled.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+
+export const MoveBtn = styled(Button)`
+  margin: 10px;
+  background-color: #009223;
+  color: #ffce32;
+  width: 30%;
+  &:hover {
+    color: #009223;
+    background-color: #ffce32;
+  }
+`;
