d078b1ec98fe1771de7be099a156cf1ade2e3d22
BobbyKJH <rlatnguscjsw@gmail.com>
2022-12-17 00:16:20 +0900

    Feat: MenuPage.tsx Create
    1) 내용
    
    -  메뉴 페이지 완성
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

---
+++ src/pages/MenuPage.tsx
@@ -0,0 +1,11 @@
+import MenuList from "../components/page/menu/MenuList";
+
+const MenuPage = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      <MenuList />
+    </div>
+  );
+};
+
+export default MenuPage;
