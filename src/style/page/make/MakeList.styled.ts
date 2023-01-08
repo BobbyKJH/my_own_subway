aa0d4a621bd0e62de4078b24851f062ebdd08ba7
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-09 06:43:24 +0900

    Feat: MakeList.styled.ts
    1) 내용
    
    - MakeList Style 생성
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/style/page/make/MakeList.styled.ts
+++ src/style/page/make/MakeList.styled.ts
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
+import { Grid } from "@mui/material";
 
-export const MakeListContainer = styled.div`
+export const MakeListContainer = styled(Grid)`
+  position: relative;
   display: flex;
   text-align: center;
 `;
